0ee5db554656d28c84cdbf3d2b7d4e822d002fa7
caledien <107306067+caledien@users.noreply.github.com>
2023-01-08 16:31:31 +0300

    Update settings.py

--- ChatDetectionSystem/settings.py
+++ ChatDetectionSystem/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -131,5 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
 MEDIA_ROOT = 'media'
 MEDIA_URL = '/media/'
-STATIC_ROOT = 'static'
+#STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
