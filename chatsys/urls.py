6bd536cdd82a5128b165aab656d95146c43b506b
caledien <107306067+caledien@users.noreply.github.com>
2023-01-08 20:05:22 +0300

    Update urls.py

--- chatsys/urls.py
+++ chatsys/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('getMessages/<str:rec>/', views.getMessages, name='getMessages'),
     path('sendMessage/<str:rec>/', views.sendMessage, name='sendMessage'),
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="templates/chatsys/reset_password.html"), name="reset_password"),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="chatsys/reset_password.html"), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
