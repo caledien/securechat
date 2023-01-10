863bc7112b98daacb0f982de639ee75a15fc4168
caledien <107306067+caledien@users.noreply.github.com>
2023-01-10 05:11:22 +0300

    Add files via upload

---
+++ ban/admin.py
@@ -0,0 +1,63 @@
+from datetime import datetime, timedelta, timezone
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+
+from ban.models import Ban, Warn
+
+
+USER_MODEL = get_user_model()
+
+
+class ExtendedUserAdmin(UserAdmin):
+    actions = [
+        'ban_selected_users_permanently',
+        'ban_selected_users_for_month',
+        'ban_selected_users_for_week',
+        'ban_selected_users_for_day',
+        'warn_selected_users',
+    ]
+
+    def warn_selected_users(self, request, queryset):
+        for user in queryset:
+            Warn.objects.create(receiver=user, creator=request.user)
+            
+        self.message_user(request, "Successfully warned selected users.")
+
+    def ban_selected_users_permanently(self, request, queryset):
+        for user in queryset:
+            Ban.objects.create(receiver=user, creator=request.user)
+        self.message_user(request, "Successfully banned selected users permanently.")
+
+    def ban_selected_users_for_month(self, request, queryset):
+        self._ban(request, queryset, 30)
+        self.message_user(request, "Successfully banned selected users for a month.")
+
+    def ban_selected_users_for_week(self, request, queryset):
+        self._ban(request, queryset, 7)
+        self.message_user(request, "Successfully banned selected users for a week.")
+
+    def ban_selected_users_for_day(self, request, queryset):
+        self._ban(request, queryset, 1)
+        self.message_user(request, "Successfully banned selected users for a day.")
+
+    def _ban(self, request, queryset, days):
+        end_date = datetime.now(timezone.utc) + timedelta(days=days)
+        for user in queryset:
+            Ban.objects.create(receiver=user, creator=request.user, end_date=end_date)
+
+
+class BanAdmin(admin.ModelAdmin):
+    list_display = ('receiver', 'end_date', 'creator')
+
+
+class WarnAdmin(admin.ModelAdmin):
+    list_display = ('receiver', 'creator')
+
+
+admin.site.unregister(USER_MODEL)
+admin.site.register(USER_MODEL, ExtendedUserAdmin)
+
+admin.site.register(Ban, BanAdmin)
+admin.site.register(Warn, WarnAdmin)
---
+++ ban/models.py
@@ -0,0 +1,61 @@
+from datetime import datetime, timezone
+
+from django.conf import settings
+from django.db import models
+from django.db.models import Q
+from django.db.models.signals import pre_save, post_delete
+from django.dispatch import receiver
+
+
+USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+
+
+class Ban(models.Model):
+    
+    
+    receiver = models.ForeignKey(USER_MODEL, unique=False,on_delete=models.CASCADE)
+    creator = models.ForeignKey(USER_MODEL, related_name='ban_creator', null=True, blank=True, default=None, on_delete=models.CASCADE)
+    end_date = models.DateTimeField(null=True, blank=True, default=None)
+
+
+class Warn(models.Model):
+    receiver = models.ForeignKey(USER_MODEL,on_delete=models.CASCADE)
+    creator = models.ForeignKey(USER_MODEL, related_name='warn_creator',on_delete=models.CASCADE)
+
+
+@receiver(pre_save, sender=Ban)
+def pre_save_ban(sender, instance, **kwargs):
+    try:
+        ban = Ban.objects.get(receiver=instance.receiver)
+        if instance.end_date is None or instance.end_date > ban.end_date:
+            ban.delete()
+        else:
+            instance.delete()
+    except Ban.DoesNotExist:
+        pass
+
+    instance.receiver.is_active = False
+
+
+@receiver(post_delete, sender=Ban)
+def post_delete_ban(sender, instance, **kwargs):
+    instance.receiver.is_active = True
+
+
+@receiver(pre_save, sender=Warn)
+def pre_save_warn(sender, instance, **kwargs):
+    threshold = getattr(settings, 'WARNS_THRESHOLD', None)
+
+    if threshold:
+        warns = Warn.objects.filter(receiver=instance.receiver)
+
+        if warns.count() >= threshold-1:
+            Ban.objects.create(receiver=instance.receiver)
+            warns.delete()
+            instance.delete()
+
+    if instance:
+        now = datetime.now(timezone.utc)
+        bans = Ban.objects.filter(receiver=instance.receiver).filter(Q(end_date__isnull=True) | Q(end_date__gt=now))
+        if bans.count() > 0:
+            instance.delete()
